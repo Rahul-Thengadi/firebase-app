1f2f3f6ed9d37d5201f73a83dfed34d24d29845e
Rahul <thengadir@gmail.com>
2022-02-08 21:52:06 +0530

    initial commit

---
+++ FirebaseApp/FirebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by Rahul Thengadi on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
